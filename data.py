a9d8f8b5d217bbcc0dd17ff21c002fa08dede45e
valter.hudovernik@gmail.com <valter.hudovernik@gmail.com>
2018-03-19 20:12:21 +0100

    Dictionary changed

--- data.py
+++ data.py
@@ -6,16 +6,16 @@ with open('html/html-procedures.txt', 'r') as file:
 soup = BeautifulSoup(html,"html.parser")
 divs = soup.find_all('div',{"class":"option"})
 
-options = []
+procedures = {}
 for d in divs:
     ids = d.attrs
     val = d.text
-    o = ids['data-value'] + " : " + val
-    options.append(o)
-
+    n = int(ids['data-value'])
+    procedures[val] = n
+    
 f = open('procedures.txt','w')
-for o in options:
-    f.write(o + '\n')
+for p in procedures:
+    f.write(p + " : " + str(procedures[p]) +'\n')
 f.close()
 print("procedures done")
 
@@ -25,14 +25,14 @@ with open('html/html-regions.txt', 'r') as file:
 soup = BeautifulSoup(html,"html.parser")
 re = soup.find_all('option')
 
-regions = []
+regions = {}
 i = 0
 for r in re:
-    regions.append(str(i) + " : " + r.text)
+    regions[r.text] = i
     i+=1
     
 f = open('regions.txt','w')
 for r in regions:
-    f.write(r + '\n')
+    f.write(r + " : " + str(regions[r]) + '\n')
 f.close()
 print("regions done")
