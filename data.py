604f937c82af5142ec585333e1b699a1d78cb92a
unknown <valter.hudovernik@gmail.com>
2018-04-05 17:11:37 +0200

    Updated data

--- data.py
+++ data.py
@@ -1,38 +1,32 @@
 from bs4 import BeautifulSoup
+import requests
+import json
 
-with open('html/html-procedures.txt', 'r') as file:
-    html=file.read().replace('\n', '')
-    
-soup = BeautifulSoup(html,"html.parser")
-divs = soup.find_all('div',{"class":"option"})
+# urgencies, regions
+url = "https://cakalnedobe.ezdrav.si/"
+html = requests.get(url).text
+soup = BeautifulSoup(html,'html.parser')
+options = soup.find_all('option')
 
-procedures = {}
-for d in divs:
-    ids = d.attrs
-    val = d.text
-    n = int(ids['data-value'])
-    procedures[val] = n
-    
-f = open('procedures.txt','w')
-for p in procedures:
-    f.write(p + " : " + str(procedures[p]) +'\n')
+urg = options[0:3]
+f = open('urgencies.txt','w')
+for u in urg:
+    f.write(u.text + " : " + str(u['value']) + '\n')
 f.close()
-print("procedures done")
+print("urgencies done")
 
-with open('html/html-regions.txt', 'r') as file:
-    html=file.read().replace('\n', '')
-
-soup = BeautifulSoup(html,"html.parser")
-re = soup.find_all('option')
-
-regions = {}
-i = 0
-for r in re:
-    regions[r.text] = i
-    i+=1
-    
+reg = options[3:len(options)-3]
 f = open('regions.txt','w')
-for r in regions:
-    f.write(r + " : " + str(regions[r]) + '\n')
+for r in reg:
+    f.write(r.text + " : " + str(r['value']) + '\n')
 f.close()
 print("regions done")
+
+# procedures
+url = "https://cakalnedobe.ezdrav.si/Home/GetProcedures"
+pro = json.loads(requests.get(url).text)
+f = open('procedures.txt','w')
+for p in pro:
+    f.write(str(p['Id']) + " : " + str(p['Name']) + '\n')
+f.close()
+print("procedures done")
